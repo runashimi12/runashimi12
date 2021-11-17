2dbd2693c5f7599272d2bf96d965d5498bd17f25
Johana <ejargoty@unicauca.edu.co>
2021-11-16 19:54:55 -0500

    heroku v5

--- core/settings.py
+++ core/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': 'db.sqlite3',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'db.sqlite3',
+#    }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -93,16 +93,16 @@ DATABASES = {
 #         'PORT' : '5432',
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE' :'django.db.backends.postgresql_psycopg2',
-#         'NAME' :'d47lnb9r0thfif',
-#         'USER'  :'cvgztmnfndazgh',
-#         'PASSWORD': '14a70b5b191c20399b0431fb5bef66592488824b9c2ff4841026679896a6d453',
-#         'HOST' :'ec2-44-194-54-186.compute-1.amazonaws.com',
-#         'PORT' :'5432',
-#         }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE' :'django.db.backends.postgresql_psycopg2',
+        'NAME' :'d47lnb9r0thfif',
+        'USER'  :'cvgztmnfndazgh',
+        'PASSWORD': '14a70b5b191c20399b0431fb5bef66592488824b9c2ff4841026679896a6d453',
+        'HOST' :'ec2-44-194-54-186.compute-1.amazonaws.com',
+        'PORT' :'5432',
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
